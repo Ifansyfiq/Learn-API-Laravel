7e48e971dae6d0bfa297e333653c6d4b96eb8cdd
Hayai <108877345+Ifansyfiq@users.noreply.github.com>
2024-09-10 22:11:13 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,12 @@ use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/logout', [AuthenticationController::class, 'logout']);
+    Route::get('me', [AuthenticationController::class, 'me']);
+});
+
 Route::get('/posts', [PostController::class, 'index']);
 Route::get('/posts/{id}', [PostController::class, 'show']);
 
 Route::post('/login', [AuthenticationController::class, 'login']);
-Route::get('/logout', [AuthenticationController::class, 'logout'])->middleware(['auth:sanctum']);
-
-Route::get('me', [AuthenticationController::class, 'me'])->middleware(['auth:sanctum']);
